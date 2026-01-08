e0d18188ca0288cb7da2377970c21e83c36bb64a
Tushar <tusharkantisingha2601@gmail.com>
2026-01-09 00:30:40 +0530

    initial setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="TUshar Kanti Singha",
+    author_email="tusharkantisingha2601@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
